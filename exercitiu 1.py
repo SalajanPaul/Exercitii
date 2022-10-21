b01b4c5f4c0421122ecedac8398ec3f93da21dfb
SalajanPaul <salajanpaul1@gmail.com>
2022-10-21 13:24:18 +0300

    da

---
+++ exercitiu 1.py
@@ -0,0 +1,7 @@
+character_name = "Bob"
+character_age = "40 de ani"
+
+print("Era odata un om pe nume " +character_name+ ",")
+print("El avea " +character_age+ ",")
+print("Chiar ii placea numele " +character_name+ ",")
+print("Dar nu ii placea ca avea " +character_age+". ")
